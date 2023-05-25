ffeb0a2afca96766c0d6acf9bb3d7e63c526613c
Kai Zickmann <kai.zickmann@zeiss.com>
2023-05-25 16:44:34 +0200

    Added action method for solved task.

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Task } from '../domain/task';
 import { Operation } from '../domain/operation';
+import { Answer } from '../domain/answer';
+import { TaskProducerService } from '../task-producer.service';
 
 @Component({
   selector: 'app-task',
@@ -18,12 +20,43 @@ export class TaskComponent {
 
   userInput: string = "";
 
-  public getOpSign(operation: Operation): string {
+  constructor(private taskProducerService: TaskProducerService) { }
+
+  ngOnInit(): void {
+    this.createTask();
+  }
+
+  createTask(): void {
+    this.task = this.taskProducerService.createTask({
+      operation: Operation.MULTIPLICATION,
+      lowerBoundary: 3,
+      higherBoundary: 5
+    });
+  }
+
+  getOpSign(operation: Operation): string {
     switch (operation) {
       case Operation.ADDITION: return "+";
       case Operation.SUBTRACTION: return "-";
       case Operation.MULTIPLICATION: return "•";
       case Operation.DIVISION: return ":";
+    }
+  }
+
+  solved(): void {
+    if (this.userInput) {
+      let userAnswer: number = Number(this.userInput);
+      if (isNaN(userAnswer)) {
+        alert("Das kann ich nicht als Zahl erkennen: " + this.userInput);
+      } else {
+        // eigentlich Logik
+          let answer: Answer = {
+            task: this.task,
+            answer: userAnswer,
+            durationMilli: 0
+          }
+          // TODO: answer weitergeben
+      }
     }
   }
 }
