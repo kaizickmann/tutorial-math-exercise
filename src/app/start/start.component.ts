13f5a74812677f6d25147f9bda34bd0b6b2dbd05
Kai Zickmann <kai.zickmann@zeiss.com>
2023-05-26 15:46:09 +0200

    Start component implemented.

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Operation } from '../domain/operation';
 
 @Component({
   selector: 'app-start',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./start.component.css']
 })
 export class StartComponent {
+
+  operations = [
+    { "checked": true, "value": Operation.ADDITION, "name": "plus - Addition" },
+    { "checked": true, "value": Operation.SUBTRACTION, "name": "minus - Subtraktion" },
+    { "checked": false, "value": Operation.MULTIPLICATION, "name": "mal - Multiplikation" },
+    { "checked": false, "value": Operation.DIVISION, "name": "durch - Division" },
+  ]
+  taskCount: number = 10;
+  operandMax: number = 20;
+
+  changeOpItem(opItem: any, event: any): void {
+    opItem.checked = event.target.checked;
+  }
+
+  getSelectedOperations(): Operation[] {
+    return this.operations.filter(item => item.checked).map(item => item.value);
+  }
+
+  isStartable(): boolean {
+    return this.getSelectedOperations().length > 0;
+  }
+
+  startTasks(): void {
+
+
+    for (let selectedName of this.operations.filter(item => item.checked).map(item => item.name)) {
+      alert(selectedName);
+    }
+    alert("Anzahl der Aufgaben: " + this.taskCount);
+    alert("maxOperator: " + this.operandMax);
+
+
+
+  }
 
 }
