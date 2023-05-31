ff402080cb1ffd0430cffb63fe5fbe05d975f818
Kai Zickmann <kai.zickmann@zeiss.com>
2023-05-31 13:22:35 +0200

    Implemented the result view.

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Mission } from '../domain/mission';
+import { LifecycleService } from '../lifecycle.service';
+import { Answer } from '../domain/answer';
 
 @Component({
   selector: 'app-result',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent {
+
+  constructor(private lifecycleService: LifecycleService) { }
+
+  public content(): Mission | undefined {
+    return this.lifecycleService.mission;
+  }
+
+  public answers(): Answer[] {
+    return this.content()?.answers || [];
+  }
+
+  public styleFor(answer: Answer) : string{
+    return this.isCorrect(answer) ? "correct" : "wrong";
+  }
+
+  public isCorrect(answer: Answer): boolean {
+    return answer.answer == answer.task.result;
+  }
 
 }
